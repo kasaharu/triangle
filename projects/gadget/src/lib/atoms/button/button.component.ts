d27d6d14a408ca0d8b34a83e93896d99b47155d1
Wataru.Kasahara <kasahara1019@gmail.com>
2018-06-29 16:13:22 +0900

    [Issue#25] Fix lint error: Missing trailing comma

--- projects/gadget/src/lib/atoms/button/button.component.ts
+++ projects/gadget/src/lib/atoms/button/button.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'gad-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent implements OnInit {
 
