2917a23107646ce603abc61afb3938c912f2230c
Wataru.Kasahara <kasahara1019@gmail.com>
2018-07-02 21:26:49 +0900

    [Issue#31] Fix e2e-gh

--- projects/gh-render-e2e/src/app.e2e-spec.ts
+++ projects/gh-render-e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to gh-render!');
+    expect(page.getParagraphText()).toEqual('Welcome to gh!');
   });
 });
