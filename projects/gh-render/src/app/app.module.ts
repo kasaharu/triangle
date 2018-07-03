9764d87c0aeb1187c5d6485b7d7cbbc3f06da253
Wataru.Kasahara <kasahara1019@gmail.com>
2018-07-03 22:36:14 +0900

    [Issue#36] Generate AppRoutingModule under the gh-render (using CLI Command)

--- projects/gh-render/src/app/app.module.ts
+++ projects/gh-render/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { UserComponent } from './components/user/user.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
