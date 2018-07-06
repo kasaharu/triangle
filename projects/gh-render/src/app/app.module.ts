52f7357e8da833a4a203f66ac39c1e24685aa2c4
Wataru.Kasahara <kasahara1019@gmail.com>
2018-07-06 14:14:59 +0900

    [Issue#36] HttpClient を使うために AppModule で Module を import

--- projects/gh-render/src/app/app.module.ts
+++ projects/gh-render/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
@@ -13,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
