6599434b2983dafded3ec86a67a5cdd4fc3ad0db
Wataru.Kasahara <kasahara1019@gmail.com>
2018-07-06 17:18:15 +0900

    [Issue#36] UserList を取得するメソッドを作成

--- projects/gh-render/src/app/services/user.service.ts
+++ projects/gh-render/src/app/services/user.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+  readonly targetUrl = 'https://api.github.com/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  fetchGitHubUserList(): Observable<User[]> {
+    return this.http.get<User[]>(this.targetUrl);
+  }
 }
