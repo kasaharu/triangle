7563b3b4360c42922103da4f55957bf2f6195138
Wataru.Kasahara <kasahara1019@gmail.com>
2018-07-08 15:22:33 +0900

    [Issue#45] Generate UserDetailComponent under the gh-render (using CLI Command)

--- projects/gh-render/src/app/app.module.ts
+++ projects/gh-render/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { UseListComponent } from './components/user-list/user-list.component';
 import { AppRoutingModule } from './app-routing.module';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UseListComponent,
+    UserDetailComponent,
   ],
   imports: [
     BrowserModule,
