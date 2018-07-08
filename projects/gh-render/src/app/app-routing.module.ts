080abcfb43d0406c298e4268b74c24d4c0254dd1
Wataru.Kasahara <kasahara1019@gmail.com>
2018-07-08 21:48:44 +0900

    temp

--- projects/gh-render/src/app/app-routing.module.ts
+++ projects/gh-render/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { UseListComponent } from './components/user-list/user-list.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/user-list', pathMatch: 'full' },
   { path: 'user-list', component: UseListComponent },
+  { path: 'user-detail/:name', component: UserDetailComponent },
 ];
 
 @NgModule({
--- projects/gh-render/src/app/components/user-detail/user-detail.component.ts
+++ projects/gh-render/src/app/components/user-detail/user-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { UserDetail } from '../../models/user-detail';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'gh-user-detail',
@@ -6,10 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-detail.component.scss'],
 })
 export class UserDetailComponent implements OnInit {
+  userDetail: UserDetail;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private userService: UserService) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(pmap => this.fetchUserDetail(pmap.get('name')));
   }
 
+  fetchUserDetail(name: string): void {
+    // this.userService.fetchGitHubUser().subscribe(userDetail => this.userDetail = userDetail);
+    this.userService.fetchGitHubUser(name).subscribe(userDetail => console.log(userDetail));
+  }
 }
