327a884adfedd4d5cf1db2ec06a5beaf973f7dd6
Wataru.Kasahara <kasahara1019@gmail.com>
2018-07-09 21:01:34 +0900

    [Issue#52] @ngrx/store および counterReducer の import

--- projects/counter/src/app/app.module.ts
+++ projects/counter/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { StoreModule } from '@ngrx/store';
+
+import { counterReducer } from './reducers/counter';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,6 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    StoreModule.forRoot({ count: counterReducer }),
   ],
   providers: [],
   bootstrap: [AppComponent],
