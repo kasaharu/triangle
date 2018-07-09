fed884560a2ddf8a3794a66095a2b103f07b7e6b
Wataru.Kasahara <kasahara1019@gmail.com>
2018-07-09 23:27:56 +0900

    [Issue#52] dispatch 処理を実装

--- projects/counter/src/app/components/counting-page/counting-page.component.ts
+++ projects/counter/src/app/components/counting-page/counting-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { CounterState } from '../../models/counter-state';
+import { INCREMENT, DECREMENT, RESET } from '../../reducers/counter';
 
 @Component({
   selector: 'cnt-counting-page',
@@ -6,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./counting-page.component.scss'],
 })
 export class CountingPageComponent implements OnInit {
+  count$: Observable<number>;
 
-  constructor() { }
+  constructor(private store: Store<CounterState>) {
+    this.count$ = store.pipe(select('count'));
+  }
 
   ngOnInit() {
   }
 
+  increment() {
+    this.store.dispatch({ type: INCREMENT });
+  }
+
+  decrement() {
+    this.store.dispatch({ type: DECREMENT });
+  }
+
+  reset() {
+    this.store.dispatch({ type: RESET });
+  }
 }
