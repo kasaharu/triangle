29c5676c52c64e008af7579d28e692e8939715ca
Wataru.Kasahara <kasahara1019@gmail.com>
2018-07-12 11:48:47 +0900

    [Issue#52] AppComponentSpec で CountingPageComponent の使用を宣言

--- projects/counter/src/app/app.component.spec.ts
+++ projects/counter/src/app/app.component.spec.ts
@@ -4,11 +4,13 @@ import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './reducers/counter';
 
 import { AppComponent } from './app.component';
+import { CountingPageComponent } from './components/counting-page/counting-page.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
+        CountingPageComponent,
       ],
       imports: [
         StoreModule.forRoot({ count: counterReducer }),
