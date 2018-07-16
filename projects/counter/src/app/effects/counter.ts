dcbb1f50fe8394776d5b8ae242297df8934b6437
Wataru.Kasahara <kasahara1019@gmail.com>
2018-07-16 15:07:26 +0900

    [Issue#52] Restore の effects を実装

--- projects/counter/src/app/effects/counter.ts
+++ projects/counter/src/app/effects/counter.ts
@@ -1,6 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Action } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
+import { mergeMap, map } from 'rxjs/operators';
+
+import * as CounterActions from '../actions/counter';
 
 @Injectable()
 export class CounterEffects {
-  constructor() {}
+  constructor(private actions$: Actions) {}
+
+  @Effect()
+  restore$: Observable<Action> = this.actions$.pipe(
+    ofType(CounterActions.CounterActionTypes.RESTORE),
+    mergeMap(_ =>
+      of(+localStorage.getItem('triangle-counter-count')).pipe(
+        map(data => new CounterActions.RestoreSuccess(data)),
+      ),
+    ),
+  );
 }
