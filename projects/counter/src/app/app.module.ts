4fa1c117404a58b188056d617b3fcfd337c28c73
Wataru.Kasahara <kasahara1019@gmail.com>
2018-07-17 19:44:44 +0900

    [Issue#52] effects ファイルを配置するディレクトリを変更(effects -> store)

--- projects/counter/src/app/app.module.ts
+++ projects/counter/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 
 import { counterReducer } from './store/counter.reducers';
-import { CounterEffects } from './effects/counter';
+import { CounterEffects } from './store/counter.effects';
 
 import { AppComponent } from './app.component';
 import { CountingPageComponent } from './components/counting-page/counting-page.component';
