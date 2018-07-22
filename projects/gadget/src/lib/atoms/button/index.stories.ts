3d350c4e4df40038bc83982c86713275e520471f
Wataru.Kasahara <kasahara1019@gmail.com>
2018-07-22 19:16:16 +0900

    [Issue#64] ButtonComponent に additionalClass を付与した story を追加

--- projects/gadget/src/lib/atoms/button/index.stories.ts
+++ projects/gadget/src/lib/atoms/button/index.stories.ts
@@ -12,6 +12,37 @@ storiesOf('Atoms/Button', module)
     },
   }))
   .add(
+    'is-priamary',
+    withNotes({ text: 'This button is primary' })(() => ({
+      component: ButtonComponent,
+      props: {
+        innerLabel: 'Primary Button',
+        additionalClass: 'is-primary',
+      },
+    })),
+  )
+  .add(
+    'is-danger',
+    withNotes({ text: 'This button is danger' })(() => ({
+      component: ButtonComponent,
+      props: {
+        innerLabel: 'Danger Button',
+        additionalClass: 'is-danger',
+      },
+    })),
+  )
+  .add(
+    'is-disabled',
+    withNotes({ text: 'This button is disabled' })(() => ({
+      component: ButtonComponent,
+      props: {
+        innerLabel: 'Disabled Button',
+        additionalClass: 'is-disabled',
+        clickButton: action('cannot click'),
+      },
+    })),
+  )
+  .add(
     'Click イベント',
     withNotes({ text: 'This button is clickable' })(() => ({
       component: ButtonComponent,
