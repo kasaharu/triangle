8fc44ecd587c13816f331ca5b8a737370cec9066
Wataru.Kasahara <kasahara1019@gmail.com>
2018-08-28 21:41:08 +0900

    [Issue#141] Generate Page2Component

--- projects/anything/src/app/app.module.ts
+++ projects/anything/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Page1Component } from './components/page1/page1.component';
+import { Page2Component } from './components/page2/page2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Page1Component,
+    Page2Component,
   ],
   imports: [
     BrowserModule,
