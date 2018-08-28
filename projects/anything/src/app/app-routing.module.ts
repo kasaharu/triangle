5f6c03b2b169efecc3a206197c9132559ba268d8
Wataru.Kasahara <kasahara1019@gmail.com>
2018-08-28 22:15:16 +0900

    [Issue#141] ルーティングの設定

--- projects/anything/src/app/app-routing.module.ts
+++ projects/anything/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { Page1Component } from './components/page1/page1.component';
+import { Page2Component } from './components/page2/page2.component';
+import { Page3Component } from './components/page3/page3.component';
+
+const routes: Routes = [
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component },
+  { path: 'page3', component: Page3Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
