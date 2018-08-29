f802cdf28c7752dd1bbba6774412be03372e8a37
Wataru.Kasahara <kasahara1019@gmail.com>
2018-08-29 11:24:33 +0900

    [Issue#143] Generate LinkComponent by CLI command

--- projects/gadget/src/lib/gadget.module.ts
+++ projects/gadget/src/lib/gadget.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { GadgetComponent } from './gadget.component';
 import { ButtonComponent } from './atoms/button/button.component';
+import { LinkComponent } from './atoms/link/link.component';
 
 @NgModule({
   imports: [
   ],
-  declarations: [GadgetComponent, ButtonComponent],
+  declarations: [GadgetComponent, ButtonComponent, LinkComponent],
   exports: [GadgetComponent, ButtonComponent],
 })
 export class GadgetModule { }
