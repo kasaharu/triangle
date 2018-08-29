091f6e46c4202606ef8e0a24d1bbdb475e288c7a
Wataru.Kasahara <kasahara1019@gmail.com>
2018-08-30 00:13:50 +0900

    [Issue#145] AppComponent で ActivatedRoute の data を購読

--- projects/anything/src/app/app.component.ts
+++ projects/anything/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'anytn-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'anything';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    console.log('AppComponent - ngOnInit');
+    this.route.data.subscribe(data => {
+      console.log(data);
+    });
+  }
 }
