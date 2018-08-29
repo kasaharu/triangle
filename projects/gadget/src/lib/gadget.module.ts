91fc4fd2774ce894c06a3a12cb4553d242195919
Wataru.Kasahara <kasahara1019@gmail.com>
2018-08-29 15:41:55 +0900

    [Issue#143] LinkComponent で routerLink を使用するために RouterModule をインポート

--- projects/gadget/src/lib/gadget.module.ts
+++ projects/gadget/src/lib/gadget.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { GadgetComponent } from './gadget.component';
 import { ButtonComponent } from './atoms/button/button.component';
 import { LinkComponent } from './atoms/link/link.component';
 
+const routes: Routes = [];
+
 @NgModule({
-  imports: [
-  ],
+  imports: [RouterModule.forRoot(routes)],
   declarations: [GadgetComponent, ButtonComponent, LinkComponent],
   exports: [GadgetComponent, ButtonComponent, LinkComponent],
 })
