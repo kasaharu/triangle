16175eac201bba75f7113769b127d798e437695f
Wataru.Kasahara <kasahara1019@gmail.com>
2018-08-29 20:16:09 +0900

    [Issue#143] a タグに表示するテキストを Input デコレーターで受け取るように変更

--- projects/gadget/src/lib/atoms/link/link.component.ts
+++ projects/gadget/src/lib/atoms/link/link.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class LinkComponent implements OnInit {
   @Input() linkPath: string;
+  @Input() linkText: string;
 
   constructor() { }
 
