da85997e33f658cff5d81725b18d91458bd7a66c
Wataru.Kasahara <kasahara1019@gmail.com>
2018-09-04 20:50:24 +0900

    [Issue#143] routerLink を Storybook で認識できるように LinkComponentStory で RouterTestingModule をインポート

--- projects/gadget/src/lib/atoms/link/link.component.stories.ts
+++ projects/gadget/src/lib/atoms/link/link.component.stories.ts
@@ -1,6 +1,7 @@
 import { storiesOf } from '@storybook/angular';
 import { withNotes } from '@storybook/addon-notes';
 import { action } from '@storybook/addon-actions';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { LinkComponent } from './link.component';
 
@@ -8,4 +9,5 @@ storiesOf('Atoms/Link', module)
   .add('ラベル表示', () => ({
     component: LinkComponent,
     props: { linkText: 'Default Link', linkPath: '/' },
+    moduleMetadata: { imports: [RouterTestingModule] },
   }));
