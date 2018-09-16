d62f731a24be45baf5b1d3dadd2ed3896a45b45a
Wataru.Kasahara <kasahara1019@gmail.com>
2018-09-16 14:10:50 +0900

    [Issue#158] Generate CounterStoreModule by CLI command

--- projects/counter/src/app/root-store/root-store.module.ts
+++ projects/counter/src/app/root-store/root-store.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CounterStoreModule } from './counter-store/counter-store.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    CounterStoreModule,
   ],
   declarations: [],
 })
