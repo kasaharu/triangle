4e55e168d402e90b49e5cee563700302b5e8849d
Wataru.Kasahara <kasahara1019@gmail.com>
2018-09-16 18:47:04 +0900

    [Issue#158] RootStoreModule で StoreModule と EffectsModule をインポート

--- projects/counter/src/app/root-store/root-store.module.ts
+++ projects/counter/src/app/root-store/root-store.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 import { CounterStoreModule } from './counter-store/counter-store.module';
 
 @NgModule({
   imports: [
     CommonModule,
     CounterStoreModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
   ],
   declarations: [],
 })
