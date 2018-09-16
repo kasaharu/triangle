dd152340901d83f1c01aa3f3648745a4297cf9c9
Wataru.Kasahara <kasahara1019@gmail.com>
2018-09-16 17:30:26 +0900

    [Issue#158] CounterStoreModule で StoreModule と EffectsModule をインポート

--- projects/counter/src/app/root-store/counter-store/counter-store.module.ts
+++ projects/counter/src/app/root-store/counter-store/counter-store.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { CounterEffects } from './effects';
+import { counterReducer } from './reducer';
+
 
 @NgModule({
   imports: [
     CommonModule,
+    StoreModule.forFeature('counter', counterReducer),
+    EffectsModule.forFeature([CounterEffects]),
   ],
-  declarations: [],
+  providers: [CounterEffects],
 })
-export class CounterStoreModule { }
+export class CounterStoreModule {}
