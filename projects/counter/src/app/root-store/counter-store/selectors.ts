dc82afae6db42f47465061a279857eb0748423e6
Wataru.Kasahara <kasahara1019@gmail.com>
2018-09-16 17:19:52 +0900

    [Issue#158] CounterStore の selector を用意

---
+++ projects/counter/src/app/root-store/counter-store/selectors.ts
@@ -0,0 +1,8 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+
+import { State } from './state';
+
+const getCounter = (state: State) => state.counter;
+
+export const selectCounterState = createFeatureSelector<State>('counter');
+export const selectCounter = createSelector(selectCounterState, getCounter);
