3bb8feed66f243ca01ed06844fa8473e0c134102
Wataru.Kasahara <kasahara1019@gmail.com>
2018-10-27 14:51:15 +0900

    [Issue#98][gh] UseListComponent#fetchUserList のテスト追加

--- projects/gh-render/src/app/components/user-list/user-list.component.spec.ts
+++ projects/gh-render/src/app/components/user-list/user-list.component.spec.ts
@@ -1,11 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of as observableOf } from 'rxjs';
 
 import { UseListComponent } from './user-list.component';
+import { UserService } from '../../services/user.service';
 
 describe('UseListComponent', () => {
   let component: UseListComponent;
   let fixture: ComponentFixture<UseListComponent>;
+  let userServive: UserService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,9 +22,21 @@ describe('UseListComponent', () => {
     fixture = TestBed.createComponent(UseListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    userServive = TestBed.get(UserService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#fetchUserList', () => {
+    const userList = [
+      { id: 1, login: 'test' },
+    ];
+
+    spyOn(userServive, 'fetchGitHubUserList').and.returnValue(observableOf(userList));
+    component.fetchUserList();
+    expect(component.userList).toBeTruthy(userList);
   });
 });
