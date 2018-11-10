e4d67c000ed0704d9113bc04b7f19ff8c44e676a
Wataru.Kasahara <kasahara1019@gmail.com>
2018-11-10 17:38:42 +0900

    [Issue#254] ユーザーリストを表示するように修正

--- projects/after-tutorial/src/app/app.component.ts
+++ projects/after-tutorial/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { User } from './user';
+
 @Component({
   selector: 'at-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'after-tutorial';
+  users: User[] = [];
 }
