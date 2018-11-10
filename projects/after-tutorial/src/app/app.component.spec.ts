40a5919de432fe54c52de22d815a8fba300a5d45
Wataru.Kasahara <kasahara1019@gmail.com>
2018-11-10 19:04:54 +0900

    [Issue#254] 子コンポーネントの依存を無視するため NO_ERRORS_SCHEMA を追加

--- projects/after-tutorial/src/app/app.component.spec.ts
+++ projects/after-tutorial/src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed, async } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
 
@@ -7,6 +8,7 @@ describe('AppComponent', () => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
       imports: [HttpClientTestingModule],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   }));
 
