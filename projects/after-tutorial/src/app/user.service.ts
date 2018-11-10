f8fa9d3fcbbad0240050fd11aa6df10663cc0f44
Wataru.Kasahara <kasahara1019@gmail.com>
2018-11-10 19:54:11 +0900

    [Issue#254] ユーザーリストを取得するためのメソッドを UserService に実装

--- projects/after-tutorial/src/app/user.service.ts
+++ projects/after-tutorial/src/app/user.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getUsers() {
+    return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users');
+  }
 }
