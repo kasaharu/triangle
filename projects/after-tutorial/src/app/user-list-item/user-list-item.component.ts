5247811f05cfe2c2c338d8a14072467be1f6f91c
Wataru.Kasahara <kasahara1019@gmail.com>
2018-11-10 18:36:18 +0900

    [Issue#254] ユーザー情報を表示する部分をコンポーネントに切り出し

--- projects/after-tutorial/src/app/user-list-item/user-list-item.component.ts
+++ projects/after-tutorial/src/app/user-list-item/user-list-item.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+
+import { User } from '../user';
 
 @Component({
   selector: 'at-user-list-item',
   templateUrl: './user-list-item.component.html',
   styleUrls: ['./user-list-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserListItemComponent implements OnInit {
+  @Input()
+  user!: User;
   constructor() {}
 
   ngOnInit() {}
