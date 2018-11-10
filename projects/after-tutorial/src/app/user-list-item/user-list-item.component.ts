ff3c0b9faca07a0ae36d4ab23ddf9e1393da16a0
Wataru.Kasahara <kasahara1019@gmail.com>
2018-11-10 18:20:42 +0900

    [Issue#254] Format by Prettier

--- projects/after-tutorial/src/app/user-list-item/user-list-item.component.ts
+++ projects/after-tutorial/src/app/user-list-item/user-list-item.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'at-user-list-item',
   templateUrl: './user-list-item.component.html',
-  styleUrls: ['./user-list-item.component.scss']
+  styleUrls: ['./user-list-item.component.scss'],
 })
 export class UserListItemComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
