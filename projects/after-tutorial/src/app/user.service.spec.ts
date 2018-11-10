224391bb0a3a7ed23b56fb6d6d496c47c768d7a9
Wataru.Kasahara <kasahara1019@gmail.com>
2018-11-10 19:59:58 +0900

    [Issue#254] UserService#getUsers のテスト作成

--- projects/after-tutorial/src/app/user.service.spec.ts
+++ projects/after-tutorial/src/app/user.service.spec.ts
@@ -1,17 +1,31 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { UserService } from './user.service';
 
 describe('UserService', () => {
+  let httpTestingController: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
+
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   it('should be created', () => {
     const service: UserService = TestBed.get(UserService);
     expect(service).toBeTruthy();
+  });
+
+  it('#getUsers', () => {
+    const service: UserService = TestBed.get(UserService);
+    const targetUrl = 'https://jsonplaceholder.typicode.com/users';
+    service.getUsers().subscribe();
+    const req = httpTestingController.expectOne(targetUrl);
+
+    expect(req.request.method).toEqual('GET');
+    expect(req.request.url).toEqual(targetUrl);
   });
 });
