c004290099ffd3718a531a28853d8c16f5ba2582
Wataru.Kasahara <kasahara1019@gmail.com>
2018-11-11 14:32:00 +0900

    [Issue#254] UserService#getUsers の spy を用意し ngOnInit() のテストを作成

--- projects/after-tutorial/src/app/app.component.spec.ts
+++ projects/after-tutorial/src/app/app.component.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of as observableOf } from 'rxjs';
+
 import { AppComponent } from './app.component';
+import { UserService } from './user.service';
 
 describe('AppComponent', () => {
+  let userService: UserService;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
@@ -12,9 +17,24 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    userService = TestBed.get(UserService);
+  });
+
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('call ngOnInit() method', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.componentInstance;
+    const users = [{ id: '1', name: 'Leanne Graham' }];
+
+    spyOn(userService, 'getUsers').and.returnValue(observableOf(users));
+    component.ngOnInit();
+
+    expect(component.users).toEqual(users);
   });
 });
