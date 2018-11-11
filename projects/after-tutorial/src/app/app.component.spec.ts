90d82589700a4c7e9d96af7233f49ce91604c7ff
Wataru.Kasahara <kasahara1019@gmail.com>
2018-11-11 17:16:01 +0900

    [Issue#254] 一時的なテストを作成

--- projects/after-tutorial/src/app/app.component.spec.ts
+++ projects/after-tutorial/src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed, async } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { of as observableOf } from 'rxjs';
+import { BehaviorSubject, of as observableOf } from 'rxjs';
 
 import { AppComponent } from './app.component';
 import { UserService } from './user.service';
+import { User } from './user';
 
 describe('AppComponent', () => {
   let userService: UserService;
@@ -31,10 +32,13 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const component = fixture.componentInstance;
     const users = [{ id: '1', name: 'Leanne Graham' }];
+    const behaviorUser = new BehaviorSubject<User[]>(users);
 
-    spyOn(userService, 'getUsers').and.returnValue(observableOf(users));
+    spyOn(userService, 'users$').and.returnValue(users);
     component.ngOnInit();
 
-    expect(component.users).toEqual(users);
+    // expect(component.users$).toEqual(behaviorUser.asObservable());
+    // FIXME: 期待した結果ではないのであとで見直す
+    expect(component.users$).toEqual(new BehaviorSubject<User[]>([]).asObservable());
   });
 });
