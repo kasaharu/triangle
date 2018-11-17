8f2fbdd4bba33ad8dded6864141843ae6b7bf9bb
Wataru.Kasahara <kasahara1019@gmail.com>
2018-11-17 19:03:36 +0900

    [Issue#254] Store の実装

--- projects/after-tutorial/src/app/store.service.ts
+++ projects/after-tutorial/src/app/store.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { State, initialState } from './state';
 
 @Injectable({
   providedIn: 'root',
 })
-export class StoreService {
+export class Store {
+  private _state$ = new BehaviorSubject<State>(initialState);
+
   constructor() {}
+
+  update(fn: (state: State) => State) {
+    const current = this._state$.value;
+    this._state$.next(fn(current));
+  }
+
+  select<T>(selector: (state: State) => T) {
+    return this._state$.pipe(map(selector));
+  }
 }
