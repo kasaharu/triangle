7c93166430a028d207008416a433905427b75741
Wataru.Kasahara <kasahara1019@gmail.com>
2018-11-20 20:35:42 +0900

    [Issue#254] ReactiveFormsModule をインポート

--- projects/after-tutorial/src/app/app.module.ts
+++ projects/after-tutorial/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UserListItemComponent } from './user-list-item/user-list-item.component';
@@ -9,7 +10,7 @@ import { UserListFilterComponent } from './user-list-filter/user-list-filter.com
 
 @NgModule({
   declarations: [AppComponent, UserListItemComponent, UserListComponent, UserListFilterComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
