5d9bc58e41d12e87515465a6d95e34d571a0e2b4
Wataru.Kasahara <kasahara1019@gmail.com>
2018-12-01 16:30:27 +0900

    [Issue#254] HttpClientModule を使っているので spec で HttpClientTestingModule をインポート

--- projects/after-tutorial/src/app/app.component.spec.ts
+++ projects/after-tutorial/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
+      imports: [HttpClientTestingModule],
     }).compileComponents();
   }));
 
