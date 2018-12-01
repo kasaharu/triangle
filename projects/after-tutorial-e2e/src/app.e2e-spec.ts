80da72b41020f7bf4297ebbe43eebb5d472d556f
Wataru.Kasahara <kasahara1019@gmail.com>
2018-12-01 16:05:39 +0900

    [Issue#254] タイトルを追加

--- projects/after-tutorial-e2e/src/app.e2e-spec.ts
+++ projects/after-tutorial-e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to after-tutorial!');
+    expect(page.getTitleText()).toEqual('Angular After Tutorial');
   });
 });
