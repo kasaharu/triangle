b2bab4e0fba0899f2a24c5ee235662e1a4b3bee2
Wataru.Kasahara <kasahara1019@gmail.com>
2018-12-08 19:22:18 +0900

    [Issue#254] UserService 経由でユーザー一覧を取得

--- projects/after-tutorial/src/app/app.component.ts
+++ projects/after-tutorial/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 import { User } from './core/models/user';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'at-root',
@@ -11,11 +11,11 @@ import { User } from './core/models/user';
 export class AppComponent implements OnInit {
   users: User[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.http.get<User[]>('https://jsonplaceholder.typicode.com/users').subscribe((data) => {
-      this.users = data;
+    this.userService.getUsers().subscribe((users) => {
+      this.users = users;
     });
   }
 }
