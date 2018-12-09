d5b8edcc826f1307006bb5d90e650d48e029c849
Wataru.Kasahara <kasahara1019@gmail.com>
2018-12-09 15:15:52 +0900

    [Issue#254] フィルタリングのための State を定義

---
+++ projects/after-tutorial/src/app/state.ts
@@ -0,0 +1,12 @@
+import { User } from './core/models/user';
+
+export interface UserListFilter {
+  nameFilter: string;
+}
+
+export interface State {
+  userList: {
+    items: User[];
+  };
+  userListFilter: UserListFilter;
+}
