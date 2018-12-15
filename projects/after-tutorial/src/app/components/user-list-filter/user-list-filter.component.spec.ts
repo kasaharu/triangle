e74e7aabda69fea99f6c393abd86516f72281ddf
Wataru.Kasahara <kasahara1019@gmail.com>
2018-12-15 16:37:28 +0900

    [Issue#254] UserListFilterComponent の spec に ReactiveFormsModule をインポート

--- projects/after-tutorial/src/app/components/user-list-filter/user-list-filter.component.spec.ts
+++ projects/after-tutorial/src/app/components/user-list-filter/user-list-filter.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { UserListFilterComponent } from './user-list-filter.component';
 
@@ -9,6 +10,7 @@ describe('UserListFilterComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [UserListFilterComponent],
+      imports: [ReactiveFormsModule],
     }).compileComponents();
   }));
 
