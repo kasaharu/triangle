c4c673c55d0476f691a34896afa459dc3a85a184
Wataru.Kasahara <kasahara1019@gmail.com>
2018-12-15 15:32:13 +0900

    [Issue#254] フォームの作成

--- projects/after-tutorial/src/app/components/user-list-filter/user-list-filter.component.ts
+++ projects/after-tutorial/src/app/components/user-list-filter/user-list-filter.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { UserListFilter } from '../../state';
 
 @Component({
   selector: 'at-user-list-filter',
@@ -6,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list-filter.component.scss'],
 })
 export class UserListFilterComponent implements OnInit {
-  constructor() {}
+  @Input()
+  set value(value: UserListFilter) {
+    this.setFormValue(value);
+  }
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({ nameFilter: [''] });
+  }
 
   ngOnInit() {}
+
+  private setFormValue(value: UserListFilter) {
+    this.form.setValue(value);
+  }
 }
