f6d90f5b2a2df46e15592b66f3be424753d424e8
Wataru.Kasahara <kasahara1019@gmail.com>
2018-12-16 15:54:51 +0900

    [Issue#254] AppRoutingModule の実装

--- projects/after-tutorial/src/app/app-routing.module.ts
+++ projects/after-tutorial/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { UserDetailPageComponent } from './components/user-detail-page/user-detail-page.component';
+
+const routes: Routes = [
+  {
+    path: 'users/:userId',
+    component: UserDetailPageComponent,
+  },
+];
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
 })
 export class AppRoutingModule {}
