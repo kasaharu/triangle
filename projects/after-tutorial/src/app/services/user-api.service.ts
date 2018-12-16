297da095c737808b6eb54e49011a88287c36531e
Wataru.Kasahara <kasahara1019@gmail.com>
2018-12-16 14:43:36 +0900

    [Issue#254] User 情報を取得するための service を作成

--- projects/after-tutorial/src/app/services/user-api.service.ts
+++ projects/after-tutorial/src/app/services/user-api.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from '../core/models/user';
+
+const apiHost = 'https://jsonplaceholder.typicode.com';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserApiService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  async getAllUsers() {
+    return await this.http.get<User[]>(`${apiHost}/users`).toPromise();
+  }
+
+  async getUserById(id: string) {
+    return await this.http.get<User[]>(`${apiHost}/users/${id}`).toPromise();
+  }
 }
