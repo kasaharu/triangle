361ac4a8f40248e9f14b952bae36ada0c838f161
Wataru.Kasahara <kasahara1019@gmail.com>
2018-12-16 15:35:15 +0900

    [Issue#254] Generate UserDetailPageComponent by CLI command

--- projects/after-tutorial/src/app/app.module.ts
+++ projects/after-tutorial/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { UserListItemComponent } from './components/user-list-item/user-list-item.component';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { UserListFilterComponent } from './components/user-list-filter/user-list-filter.component';
+import { UserDetailPageComponent } from './components/user-detail-page/user-detail-page.component';
 
 @NgModule({
-  declarations: [AppComponent, UserListItemComponent, UserListComponent, UserListFilterComponent],
+  declarations: [AppComponent, UserListItemComponent, UserListComponent, UserListFilterComponent, UserDetailPageComponent],
   imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ projects/after-tutorial/src/app/components/user-detail-page/user-detail-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserDetailPageComponent } from './user-detail-page.component';
+
+describe('UserDetailPageComponent', () => {
+  let component: UserDetailPageComponent;
+  let fixture: ComponentFixture<UserDetailPageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UserDetailPageComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserDetailPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
