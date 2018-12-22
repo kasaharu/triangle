5add97ce000d70d941ef457c24195940dc2d1b60
Wataru.Kasahara <kasahara1019@gmail.com>
2018-12-22 14:16:38 +0900

    [Issue#350] routing で指定するトップレベルの component を container へ

--- projects/gh-render/src/app/app-routing.module.ts
+++ projects/gh-render/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { UseListComponent } from './components/user-list/user-list.component';
+import { UseListComponent } from './containers/user-list/user-list.component';
 
 const routes: Routes = [{ path: '', redirectTo: '/user-list', pathMatch: 'full' }, { path: 'user-list', component: UseListComponent }];
 
