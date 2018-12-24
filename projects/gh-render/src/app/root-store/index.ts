6569a9443c1a427595bc1de3e10883d86147ca4f
Wataru.Kasahara <kasahara1019@gmail.com>
2018-12-24 18:32:42 +0900

    [Issue#353] RootStoreModule で barrel export

---
+++ projects/gh-render/src/app/root-store/index.ts
@@ -0,0 +1,6 @@
+import { RootStoreModule } from './root-store.module';
+import * as RootStoreState from './root-state';
+
+export * from './user-store';
+
+export { RootStoreModule, RootStoreState };
