cff78977f1fcaec195e1be459204049bfc53b050
Wataru.Kasahara <kasahara1019@gmail.com>
2018-12-24 18:14:52 +0900

    [Issue#353] UserStoreModule で StoreModule のインポート

--- projects/gh-render/src/app/root-store/user-store/user-store.module.ts
+++ projects/gh-render/src/app/root-store/user-store/user-store.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+
+import { userReducer } from './reducers';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, StoreModule.forFeature('user', userReducer)],
 })
 export class UserStoreModule {}
