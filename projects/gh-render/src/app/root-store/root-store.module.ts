176f851245f2d085c8f06f61f25f2fb4a385a2ad
Wataru.Kasahara <kasahara1019@gmail.com>
2018-12-24 16:32:33 +0900

    [Issue#353] Generate UserStoreModule by CLI command

--- projects/gh-render/src/app/root-store/root-store.module.ts
+++ projects/gh-render/src/app/root-store/root-store.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserStoreModule } from './user-store/user-store.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, UserStoreModule],
 })
 export class RootStoreModule {}
