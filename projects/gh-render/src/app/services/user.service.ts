8f62bac3fbc9adde030c84fb2675ca222dec7c01
Wataru.Kasahara <kasahara1019@gmail.com>
2018-12-24 16:46:57 +0900

    [Issue#353] Models のディレクトリ構成を変更

--- projects/gh-render/src/app/services/user.service.ts
+++ projects/gh-render/src/app/services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { User } from '../models/user';
+import { User } from '../core/models';
 
 @Injectable({
   providedIn: 'root',
