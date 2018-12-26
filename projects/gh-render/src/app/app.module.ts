7be342a018ce3b56507c40fda06756c4876dda16
Wataru.Kasahara <kasahara1019@gmail.com>
2018-12-26 21:49:13 +0900

    [Issue#358] AppModule で RootStoreModule をインポート

--- projects/gh-render/src/app/app.module.ts
+++ projects/gh-render/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { RootStoreModule } from './root-store';
+
 import { AppComponent } from './app.component';
 import { UseListComponent } from './containers/user-list/user-list.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, UseListComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, RootStoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
