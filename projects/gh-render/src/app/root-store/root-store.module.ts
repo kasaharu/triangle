c59fa9c4fdbb6c941fd58531536fec9062c5b255
Wataru.Kasahara <kasahara1019@gmail.com>
2018-12-26 21:48:40 +0900

    [Issue#358] RootStore で StoreModule をインポート

--- projects/gh-render/src/app/root-store/root-store.module.ts
+++ projects/gh-render/src/app/root-store/root-store.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+
 import { UserStoreModule } from './user-store/user-store.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, UserStoreModule],
+  imports: [CommonModule, StoreModule.forRoot({}), UserStoreModule],
 })
 export class RootStoreModule {}
