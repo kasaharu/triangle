49417c6015f73477c9848604d18dd3efd24c9415
Wataru.Kasahara <kasahara1019@gmail.com>
2018-12-28 09:21:46 +0900

    [Issue#358] EffectsModule インポート

--- projects/gh-render/src/app/root-store/root-store.module.ts
+++ projects/gh-render/src/app/root-store/root-store.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { UserStoreModule } from './user-store/user-store.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, StoreModule.forRoot({}), UserStoreModule],
+  imports: [CommonModule, StoreModule.forRoot({}), EffectsModule.forRoot([]), UserStoreModule],
 })
 export class RootStoreModule {}
--- projects/gh-render/src/app/root-store/user-store/user-store.module.ts
+++ projects/gh-render/src/app/root-store/user-store/user-store.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { userReducer } from './reducers';
+import { UserEffects } from './effects';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, StoreModule.forFeature('user', userReducer)],
+  imports: [CommonModule, StoreModule.forFeature('user', userReducer), EffectsModule.forFeature([UserEffects])],
 })
 export class UserStoreModule {}
