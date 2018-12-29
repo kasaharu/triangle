a68cd71802158e49433c484437ac7ef965169978
Wataru.Kasahara <kasahara1019@gmail.com>
2018-12-29 11:50:33 +0900

    [Issue#358] UserStore から user 情報を取得

--- projects/gh-render/src/app/containers/user-list/user-list.component.ts
+++ projects/gh-render/src/app/containers/user-list/user-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { RootStoreState, UserStoreActions, UserStoreSelectors } from '../../root-store';
 
 import { User } from '../../core/models';
 import { UserService } from '../../services/user.service';
@@ -10,10 +14,13 @@ import { UserService } from '../../services/user.service';
 })
 export class UseListComponent implements OnInit {
   userList: User[];
+  userList$: Observable<User[]>;
 
-  constructor(private userService: UserService) {}
+  constructor(private store$: Store<RootStoreState.State>, private userService: UserService) {}
 
   ngOnInit() {
+    this.userList$ = this.store$.pipe(select(UserStoreSelectors.selectUser));
+    this.store$.dispatch(new UserStoreActions.FetchRequestAction());
     this.fetchUserList();
   }
 
