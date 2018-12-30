6646c658f04e4b2e615aaea88d4b749b42503a20
Wataru.Kasahara <kasahara1019@gmail.com>
2018-12-30 19:56:28 +0900

    [Issue#45] Format by Prettier

--- projects/gh-render/src/app/containers/user-detail/user-detail.component.spec.ts
+++ projects/gh-render/src/app/containers/user-detail/user-detail.component.spec.ts
@@ -8,9 +8,8 @@ describe('UserDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserDetailComponent ]
-    })
-    .compileComponents();
+      declarations: [UserDetailComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
--- projects/gh-render/src/app/containers/user-detail/user-detail.component.ts
+++ projects/gh-render/src/app/containers/user-detail/user-detail.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'gh-user-detail',
   templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.scss']
+  styleUrls: ['./user-detail.component.scss'],
 })
 export class UserDetailComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
