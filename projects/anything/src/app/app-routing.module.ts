f8b8db9be2c4d92ef86539b7f7ac4769fcfa0d6c
Wataru.Kasahara <kasahara1019@gmail.com>
2019-01-26 13:55:38 +0900

    [anything] Page1 の子階層に P1Section1 を配置

--- projects/anything/src/app/app-routing.module.ts
+++ projects/anything/src/app/app-routing.module.ts
@@ -4,9 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { Page1Component } from './components/page1/page1.component';
 import { Page2Component } from './components/page2/page2.component';
 import { Page3Component } from './components/page3/page3.component';
+import { P1Section1Component } from './components/p1-section1/p1-section1.component';
 
 const routes: Routes = [
-  { path: 'page1', component: Page1Component, data: { title: 'page1-title' } },
+  {
+    path: 'page1',
+    component: Page1Component,
+    data: { title: 'page1-title' },
+    children: [{ path: 'section1', component: P1Section1Component }],
+  },
   { path: 'page2', component: Page2Component, data: { title: 'page2-title' } },
   { path: 'page3', component: Page3Component, data: { title: 'page3-title' } },
 ];
