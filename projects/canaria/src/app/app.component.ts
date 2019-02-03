5566b53b73d3ccb0f22d573c7530cf1bb77abecd
Wataru.Kasahara <kasahara1019@gmail.com>
2019-02-03 13:45:45 +0900

    [Issue#303] Format by Prettier

--- projects/canaria/src/app/app.component.ts
+++ projects/canaria/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'canaria-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'canaria';
