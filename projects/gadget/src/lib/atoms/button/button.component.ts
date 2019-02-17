ec32ce3a50253bea3993d3c9e2e589a8e7fcefbe
Wataru.Kasahara <kasahara1019@gmail.com>
2019-02-17 21:32:22 +0900

    [common][Issue#460] ButtonComponent の selector が kebab-case 以外でも許容するように変更

--- projects/gadget/src/lib/atoms/button/button.component.ts
+++ projects/gadget/src/lib/atoms/button/button.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line: component-selector
   selector: 'button[gad-button]',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
