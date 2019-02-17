d93ca353002d883bd60da6640b8447ab55e2439e
Wataru.Kasahara <kasahara1019@gmail.com>
2019-02-17 14:49:28 +0900

    [anything][Issue#458] MenuListComponent と MenuItemListComponent を使ってリストを表示

--- projects/anything/src/app/components/menu-list-item/menu-list-item.component.ts
+++ projects/anything/src/app/components/menu-list-item/menu-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'anytn-menu-list-item',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-list-item.component.scss'],
 })
 export class MenuListItemComponent implements OnInit {
+  @Input()
+  id: number;
+  @Input()
+  label: string;
   constructor() {}
 
   ngOnInit() {}
--- projects/anything/src/app/components/menu-list/menu-list.component.ts
+++ projects/anything/src/app/components/menu-list/menu-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'anytn-menu-list',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-list.component.scss'],
 })
 export class MenuListComponent implements OnInit {
+  @Input()
+  menuList: object[];
   constructor() {}
 
   ngOnInit() {}
