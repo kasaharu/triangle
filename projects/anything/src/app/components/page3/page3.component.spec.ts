e8b029316779ca23cb306c741d2e12b24c21c227
Wataru.Kasahara <kasahara1019@gmail.com>
2019-02-17 14:30:33 +0900

    [anything][Issue#458] Page3Component に MenuListComponent を配置

--- projects/anything/src/app/components/page3/page3.component.spec.ts
+++ projects/anything/src/app/components/page3/page3.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { Page3Component } from './page3.component';
 
@@ -9,6 +10,7 @@ describe('Page3Component', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [Page3Component],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   }));
 
