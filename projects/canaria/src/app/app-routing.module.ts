f404dc815775b2be21ad1b9cf00ccfb80818f4a9
Wataru.Kasahara <kasahara1019@gmail.com>
2019-02-24 16:51:09 +0900

    [canaria] HomeComponent に遷移できるように修正

--- projects/canaria/src/app/app-routing.module.ts
+++ projects/canaria/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [{ path: 'home', component: HomeComponent }];
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
