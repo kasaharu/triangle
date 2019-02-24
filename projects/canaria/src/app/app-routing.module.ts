d98f4a40fdba734d976bf91b040652fc7f5d7687
Wataru.Kasahara <kasahara1019@gmail.com>
2019-02-24 17:30:46 +0900

    [canaria] HomeComponent から ReadComponent への遷移を追加

--- projects/canaria/src/app/app-routing.module.ts
+++ projects/canaria/src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
+import { ReadComponent } from './pages/read/read.component';
 
-const routes: Routes = [{ path: '', redirectTo: '/home', pathMatch: 'full' }, { path: 'home', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'read', component: ReadComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
