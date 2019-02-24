dc8e9c3daa09ade038343bde5228db7069a85292
Wataru.Kasahara <kasahara1019@gmail.com>
2019-02-24 18:37:48 +0900

    [canaria] Format by Prettier

--- projects/canaria/src/app/components/book-list/book-list.component.ts
+++ projects/canaria/src/app/components/book-list/book-list.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'canaria-book-list',
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.scss']
+  styleUrls: ['./book-list.component.scss'],
 })
 export class BookListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
