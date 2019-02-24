6295b9fb56bfe474ecc231bf77f89fc997683ef0
Wataru.Kasahara <kasahara1019@gmail.com>
2019-02-24 16:36:57 +0900

    [canaria] Generate HomeComponent by CLI command

--- projects/canaria/src/app/app.module.ts
+++ projects/canaria/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
