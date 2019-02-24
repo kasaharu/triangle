de350e5f7213443f396cde4a93f3b26d468bdf7f
Wataru.Kasahara <kasahara1019@gmail.com>
2019-02-24 17:10:24 +0900

    [canaria] Fix lint error

--- projects/canaria/src/app/app.module.ts
+++ projects/canaria/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
