ccdc73a9feb1f7d06545b361ccf839010fc913f1
Wataru.Kasahara <kasahara1019@gmail.com>
2019-02-24 16:20:07 +0900

    [canaria] Generate AppRoutingModule by CLI command

--- projects/canaria/src/app/app.module.ts
+++ projects/canaria/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
