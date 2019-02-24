cc0475c5b8df83c63fa3dcab4bf76dcee2f9acf1
Wataru.Kasahara <kasahara1019@gmail.com>
2019-02-24 16:53:49 +0900

    [canaria] ルートパスアクセス時に /home に遷移するように変更

--- projects/canaria/src/app/app-routing.module.ts
+++ projects/canaria/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [{ path: 'home', component: HomeComponent }];
+const routes: Routes = [{ path: '', redirectTo: '/home', pathMatch: 'full' }, { path: 'home', component: HomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
