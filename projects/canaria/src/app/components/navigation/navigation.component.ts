e4b380befc94125d0330929eceac61f4aa1b22b0
Wataru.Kasahara <kasahara1019@gmail.com>
2019-02-25 22:12:57 +0900

    [canaria] Format by Prettier

--- projects/canaria/src/app/components/navigation/navigation.component.ts
+++ projects/canaria/src/app/components/navigation/navigation.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'canaria-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
