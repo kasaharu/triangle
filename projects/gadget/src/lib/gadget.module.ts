6ba29ba66a22219bfd266f6ad4b1b7c15b9dfff2
Wataru.Kasahara <kasahara1019@gmail.com>
2019-02-28 22:56:03 +0900

    [gadget] Generate HeaderComponent by CLI command

--- projects/gadget/src/lib/gadget.module.ts
+++ projects/gadget/src/lib/gadget.module.ts
@@ -4,12 +4,13 @@ import { ButtonComponent } from './atoms/button/button.component';
 import { LinkComponent } from './atoms/link/link.component';
 import { GadgetComponent } from './gadget.component';
 import { SingleColumnComponent } from './templates/single-column/single-column.component';
+import { HeaderComponent } from './organisms/header/header.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  declarations: [GadgetComponent, ButtonComponent, LinkComponent, SingleColumnComponent],
+  declarations: [GadgetComponent, ButtonComponent, LinkComponent, SingleColumnComponent, HeaderComponent],
   exports: [GadgetComponent, ButtonComponent, LinkComponent],
 })
 export class GadgetModule {}
