1627c9983f8803e95409a0a49719746167a689a8
Wataru.Kasahara <kasahara1019@gmail.com>
2019-03-02 18:25:27 +0900

    [canaria-mock] Generate BooksController by Nest CLI command

--- canaria-mock-server/src/app.module.ts
+++ canaria-mock-server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksController } from './books/books.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, BooksController],
   providers: [AppService],
 })
 export class AppModule {}
