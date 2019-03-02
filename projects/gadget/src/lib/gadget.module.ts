095efdf1a6f0535bd519eda9dc8f5b87a3afd14f
Wataru.Kasahara <kasahara1019@gmail.com>
2019-03-02 12:39:15 +0900

    [gadget] HeaderComponent, SingleColumnComponent の export

--- projects/gadget/src/lib/gadget.module.ts
+++ projects/gadget/src/lib/gadget.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ButtonComponent } from './atoms/button/button.component';
 import { LinkComponent } from './atoms/link/link.component';
 import { GadgetComponent } from './gadget.component';
-import { SingleColumnComponent } from './templates/single-column/single-column.component';
 import { HeaderComponent } from './organisms/header/header.component';
+import { SingleColumnComponent } from './templates/single-column/single-column.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   declarations: [GadgetComponent, ButtonComponent, LinkComponent, SingleColumnComponent, HeaderComponent],
-  exports: [GadgetComponent, ButtonComponent, LinkComponent],
+  exports: [GadgetComponent, ButtonComponent, LinkComponent, HeaderComponent, SingleColumnComponent],
 })
 export class GadgetModule {}
