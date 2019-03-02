135b94aeadde6b7caeac8e6a6153606fd07070bd
Wataru.Kasahara <kasahara1019@gmail.com>
2019-03-02 13:20:38 +0900

    [gadget] Format by Prettier

--- projects/gadget/src/lib/templates/two-columns/two-columns.component.spec.ts
+++ projects/gadget/src/lib/templates/two-columns/two-columns.component.spec.ts
@@ -8,9 +8,8 @@ describe('TwoColumnsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TwoColumnsComponent ]
-    })
-    .compileComponents();
+      declarations: [TwoColumnsComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
--- projects/gadget/src/lib/templates/two-columns/two-columns.component.ts
+++ projects/gadget/src/lib/templates/two-columns/two-columns.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'gad-two-columns',
   templateUrl: './two-columns.component.html',
-  styleUrls: ['./two-columns.component.scss']
+  styleUrls: ['./two-columns.component.scss'],
 })
 export class TwoColumnsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
