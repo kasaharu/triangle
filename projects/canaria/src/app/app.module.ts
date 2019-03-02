aae07e71d7b5318a7055f26ec0481d70c8a1ba53
Wataru.Kasahara <kasahara1019@gmail.com>
2019-03-02 15:36:31 +0900

    [canaria] 不要になった NavigationComponent を削除

--- projects/canaria/src/app/app.module.ts
+++ projects/canaria/src/app/app.module.ts
@@ -7,13 +7,12 @@ import { GadgetModule } from 'gadget';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookListComponent } from './components/book-list/book-list.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ReadComponent } from './pages/read/read.component';
 import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ReadComponent, BookListComponent, NavigationComponent, SidemenuComponent],
+  declarations: [AppComponent, HomeComponent, ReadComponent, BookListComponent, SidemenuComponent],
   imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GadgetModule],
   providers: [],
   bootstrap: [AppComponent],
