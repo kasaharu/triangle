8d5e2306d93e9907b51005cec939f4b9261b515f
Wataru.Kasahara <kasahara1019@gmail.com>
2019-03-02 12:38:02 +0900

    [gadget] Header の実装

--- projects/gadget/src/lib/organisms/header/header.component.ts
+++ projects/gadget/src/lib/organisms/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'gad-header',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @Input()
+  title: string;
   constructor() {}
 
   ngOnInit() {}
