05c64a237bd984bdb967673daca44d588fe22dd4
Wataru.Kasahara <kasahara1019@gmail.com>
2019-03-02 16:06:28 +0900

    [canaria] reading への遷移を追加

--- projects/canaria/src/app/app-routing.module.ts
+++ projects/canaria/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { ReadComponent } from './pages/read/read.component';
+import { ReadingComponent } from './pages/reading/reading.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'read', component: ReadComponent },
+  { path: 'reading', component: ReadingComponent },
 ];
 
 @NgModule({
