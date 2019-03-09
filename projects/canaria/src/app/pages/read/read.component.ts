e9cb8ce678ce7165afaf5eb4fc032475c472ec28
Wataru.Kasahara <kasahara1019@gmail.com>
2019-03-09 19:25:15 +0900

    [canaria] BookList を API 経由で取得するように変更

--- projects/canaria/src/app/pages/read/read.component.ts
+++ projects/canaria/src/app/pages/read/read.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
 import { Book, BookType } from '../../core/domains';
+import { BookService } from '../../services/book.service';
 
 @Component({
   selector: 'canaria-read',
@@ -10,30 +11,9 @@ import { Book, BookType } from '../../core/domains';
 })
 export class ReadComponent implements OnInit {
   bookList$: Observable<Book[]>;
-  constructor() {}
+  constructor(private bookService: BookService) {}
 
   ngOnInit() {
-    this.bookList$ = of([
-      { id: 1, name: 'book1', type: BookType.READ, isFavorite: false },
-      { id: 2, name: 'book2', type: BookType.READ, isFavorite: false },
-      { id: 3, name: 'book3', type: BookType.READ, isFavorite: false },
-      { id: 4, name: 'book4', type: BookType.READ, isFavorite: false },
-      { id: 5, name: 'book5', type: BookType.READ, isFavorite: false },
-      { id: 6, name: 'book6', type: BookType.READ, isFavorite: false },
-      { id: 7, name: 'book7', type: BookType.READ, isFavorite: false },
-      { id: 8, name: 'book8', type: BookType.READ, isFavorite: false },
-      { id: 9, name: 'book9', type: BookType.READ, isFavorite: false },
-      { id: 10, name: 'book10', type: BookType.READ, isFavorite: true },
-      { id: 11, name: 'book11', type: BookType.READ, isFavorite: true },
-      { id: 12, name: 'book12', type: BookType.READ, isFavorite: true },
-      { id: 13, name: 'book13', type: BookType.READ, isFavorite: true },
-      { id: 14, name: 'book14', type: BookType.READ, isFavorite: true },
-      { id: 15, name: 'book15', type: BookType.READ, isFavorite: true },
-      { id: 16, name: 'book16', type: BookType.READ, isFavorite: true },
-      { id: 17, name: 'book17', type: BookType.READ, isFavorite: true },
-      { id: 18, name: 'book18', type: BookType.READ, isFavorite: true },
-      { id: 19, name: 'book19', type: BookType.READ, isFavorite: true },
-      { id: 20, name: 'book20', type: BookType.READ, isFavorite: true },
-    ]);
+    this.bookList$ = this.bookService.fetchBookList();
   }
 }
