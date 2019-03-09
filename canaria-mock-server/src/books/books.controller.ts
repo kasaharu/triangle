b677deb465d34bb2125070bf4d6da82d73fe4b84
Wataru.Kasahara <kasahara1019@gmail.com>
2019-03-09 17:43:14 +0900

    [canaria-mock] type が READING の BookList を返す API を実装

--- canaria-mock-server/src/books/books.controller.ts
+++ canaria-mock-server/src/books/books.controller.ts
@@ -21,8 +21,15 @@ export class BooksController {
     return this.bookList;
   }
 
+  // curl -X GET http://localhost:3000/books/read
   @Get('/read')
   findReadBookList() {
     return this.bookList.filter(book => book.type === BookType.READ);
+  }
+
+  // curl -X GET http://localhost:3000/books/reading
+  @Get('/reading')
+  findReadingBookList() {
+    return this.bookList.filter(book => book.type === BookType.READING);
   }
 }
