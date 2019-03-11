9910d9d49e4cd86164211f172c507c281e4fd248
Wataru.Kasahara <kasahara1019@gmail.com>
2019-03-12 08:04:08 +0900

    [canaria] 新規作成ページのフォームを用意

--- projects/canaria/src/app/pages/new/new.component.ts
+++ projects/canaria/src/app/pages/new/new.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { BookType } from '../../core/domains';
 
 @Component({
   selector: 'canaria-new',
   templateUrl: './new.component.html',
-  styleUrls: ['./new.component.scss']
+  styleUrls: ['./new.component.scss'],
 })
 export class NewComponent implements OnInit {
+  bookTypeList = [BookType.NONE, BookType.READ, BookType.READING];
+  bookForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
+    this.bookForm = this.fb.group({
+      name: ['hoge'],
+      type: [this.bookTypeList[0]],
+      isFavorite: ['true'],
+    });
   }
 
+  ngOnInit() {}
 }
