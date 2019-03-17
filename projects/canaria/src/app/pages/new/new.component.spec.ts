6765019cbe4dfc1819483f8b10982b2a28761168
Wataru.Kasahara <kasahara1019@gmail.com>
2019-03-17 17:08:34 +0900

    [canaria] 新規登録ボタン押下時に発火するメソッドのテストを追加

--- projects/canaria/src/app/pages/new/new.component.spec.ts
+++ projects/canaria/src/app/pages/new/new.component.spec.ts
@@ -1,12 +1,16 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 
+import { Book, BookStatus } from '../../core/domains';
+import { BookService } from '../../services/book.service';
 import { NewComponent } from './new.component';
 
 describe('NewComponent', () => {
   let component: NewComponent;
   let fixture: ComponentFixture<NewComponent>;
+  let bookService: BookService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -14,6 +18,8 @@ describe('NewComponent', () => {
       imports: [ReactiveFormsModule],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
+
+    bookService = TestBed.get(BookService);
   }));
 
   beforeEach(() => {
@@ -24,5 +30,12 @@ describe('NewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('call clickButton()', () => {
+    const newBook: Book = { id: 1, name: 'book', status: BookStatus.NONE, isFavorite: true };
+    spyOn(bookService, 'registNewBook').and.returnValue(of(newBook));
+    component.clickButton();
+    expect(bookService.registNewBook).toHaveBeenCalled();
   });
 });
